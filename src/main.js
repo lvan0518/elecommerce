e7b9f86b9ac1e70db4fb8d77f800bcd5da12f6cc
lvan0518 <3270698153@qq.com>
2020-10-03 18:46:31 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
-
+import Elementui from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/global.css'
+import './plugins/element'
+import './assets/fonts/iconfont.css'
+Vue.prototype.$http = axios;
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
 Vue.config.productionTip = false
 
-new Vue({
+const commerce = new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
